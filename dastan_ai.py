9dc24703f121ae1291b801637d0fea7637a596b6
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2023-01-17 18:29:00 +0000

    Improve Process class parsing functions

--- dastan_ai.py
+++ dastan_ai.py
@@ -13,8 +13,30 @@ class Process:
                                         encoding='utf-8')
         self.echo = echo
 
+    def read(self, n):
+        data = self.process.stdout.read(n)
+        if self.echo:
+            print(data, end='')
+        return data
+
+    def read_to(self, end):
+        index = 0
+        data = []
+        while True:
+            char = self.read(1)
+            data.append(char)
+            if char == end[index]:
+                index += 1
+            if index == len(end):
+                break
+        return ''.join(data)
+
     def readline(self):
-        line = self.process.stdout.readline()
+        line = ''
+        while not line.strip():
+            if self.echo:
+                print(line, end='')
+            line = self.process.stdout.readline()
         if self.echo:
             print(line, end='')
         return line
@@ -54,9 +76,23 @@ def parse_grid(grid):
 
 
 process = Process('Paper1_ALvl_2023_Python3_Pub_0.0.0.py')
-process.readline()
 
 grid = []
 for _ in range(9):
     grid.append(process.readline())
-print(parse_grid(grid))
+p1_kotla, p1_pieces, p2_kotla, p2_pieces = parse_grid(grid)
+
+offer = process.readline()
+offer = Piece(re.match('Move option offer: (.+)\n', offer).group(1))
+
+player = 1 if process.readline() == 'Player One\n' else 2
+score = int(re.match('Score: (\d+)\n', process.readline()).group(1))
+queue = [Piece(name) for name in re.findall('\d\. ([a-z]+)', process.readline())]
+
+process.readline()
+process.read_to(': ')
+process.write('1')
+process.read_to(': ')
+process.write('22')
+process.read_to(': ')
+process.write('32')
